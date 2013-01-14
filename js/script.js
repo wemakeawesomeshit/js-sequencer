01ffaec378a9b001e69747859fde07c057bb65e3
Adam Howard <adz@skatty.me>
2013-01-14 00:36:06 +0000

    add an extra dimension so we can play samples

--- js/script.js
+++ js/script.js
@@ -35,78 +35,106 @@ function initCamera(callback) {
 
 }
 
-var Track = (function() {
+var TimeSlice = (function() {
 
-  function Track(name) {
-    this.name = name;
-    this.audioFiles = [];
-    
-    for (var i=1; i < 29; i++) {
-      var audioFile = new AudioFile();
-      audioFile.load('music/chunks/'+this.name+'-chunk-'+i+'.mp3');
-      this.audioFiles.push(audioFile);
-    };
-    
-    this.view = new TrackView(this);
+  function TimeSlice(samples) {
+    this.samples = samples;
+    this.activeSamples = [];
   }
   
-  Track.prototype.playAll = function() {
+  TimeSlice.prototype.playActiveSamples = function(bar) {
     var that = this;
-    var i = -1;
-    setInterval(function() {
-      that.playBar(++i % that.audioFiles.length)
-    }, 6650);
+
+    function isActive(_, i) { return !!that.activeSamples[i]; }
+    var activeSamples = _.filter(that.samples, isActive);
+    _.invoke(activeSamples, 'play');
   }
   
-  Track.prototype.playBar = function(bar) {
-    this.audioFiles[bar].play();
-    this.view.highlightFile(bar)
+  TimeSlice.prototype.updateActiveSamples = function(activeSamples) {
+    this.activeSamples = activeSamples;
   }
 
-  return Track;
+  return TimeSlice;
 })();
 
-var TrackView = (function() {
 
-  function TrackView(track) {
-    this.track = track;
-    var view = $('<div class="track"><div class="name">'+this.track.name+'</div></div>');
+var View = (function() {
+
+  function View(timeSlices) {
+    this.timeSlices = timeSlices;
+    this.render();
+    this.updateTimeSlices();
+  }
+  
+  View.prototype.render = function() {
+    var view = $('<div></div>');
     this.view = view;
-
-    var files = $('<div class="files"></div>');
-    _.each(this.track.audioFiles, function(audioFile) {
-      var audioFileView = $('<div class="audioFile"></div>');
-      audioFileView.click(function() {
-        audioFileView.toggleClass('muted');
-        audioFile.muted = audioFileView.hasClass('muted')
+    var that = this;
+    
+    _.each(this.timeSlices, function(timeSlice, i) {
+      var timeSliceView = $('<div class="timeSlice"></div>');
+      timeSliceView.click(function() {
+        currentSliceTicker = i-1;
+        // cancel and start slice
+        stopAll();
+        
+        clearInterval(playInterval);
+        playInterval = setInterval(playCurrentSlice, 6650);
+        playCurrentSlice();
       });
-      files.append(audioFileView);
+      
+      _.each(timeSlice.samples, function(sample) {
+        var audioFileView = $('<div class="sample" title="'+sample.name+'"></div>');
+        audioFileView.click(function() {
+          audioFileView.toggleClass('enabled');
+          that.updateTimeSlices();
+          return false;
+        });
+        
+        timeSliceView.append(audioFileView);
+      });
+      
+      that.view.append(timeSliceView);
+      timeSliceView.append('<span style="clear: both;">&nbsp;</span>');
+      
     });
     
-    files.append('<span style="clear: both;"></span>');
-    this.view.append(files);
-    $('#tracks').append(this.view);
+    $('#timeSlices').append(this.view);
+    
   }
   
-  TrackView.prototype.highlightFile = function(fileNumber) {
-    this.view.find('.audioFile:eq('+(fileNumber-1)+')').removeClass('highlight');
-    this.view.find('.audioFile:eq('+fileNumber+')').addClass('highlight');
-  };
+  View.prototype.updateTimeSlices = function() {
+    var that = this;
+    $('.timeSlice').each(function(timeSliceIndex) {
+      var currentTimeSlice = that.timeSlices[timeSliceIndex];
+      
+      var activeSamples = $(this).find('.sample').map(function(sampleIndex) {
+        return $(this).hasClass('enabled');
+      });
+      
+      currentTimeSlice.updateActiveSamples(activeSamples);
+    });
+  }
   
-  return TrackView;
+  View.prototype.highlightSlice = function(sliceNo) {
+    var that = this;
+    $('.timeSlice').css('background-color', 'transparent');
+    $('.timeSlice:eq('+sliceNo+')').css('background-color', 'yellow');
+  }
+  
+  return View;
 })();
 
-var AudioFile = (function() {
 
-  function AudioFile() {
-    this.muted = false;
-  }
 
-  AudioFile.prototype.load = function(file) {
+var Sample = (function() {
+
+  function Sample(file) {
+    this.name = file;
     var that = this;
         
     var request = new XMLHttpRequest();
-    request.open("GET", file, true);
+    request.open("GET", 'music/samples/'+file, true);
     request.responseType = "arraybuffer";
 
     request.onload = function() {
@@ -120,48 +148,110 @@ var AudioFile = (function() {
   
   };
   
-  AudioFile.prototype.play = function() {
-    if (this.muted) return;
+  Sample.prototype.play = function() {
     if (!this.buffer) {
       console.log('This track isn\'t ready yet. Should probably make sure we\'re all loaded first');
       return;
     };
     
-    var source = audioContext.createBufferSource();
-    source.buffer = this.buffer;              
-    source.connect(audioContext.destination);      
-    source.noteOn(0);                         
+    // Could probably do this bit on init
+    this.source = audioContext.createBufferSource();
+    this.source.buffer = this.buffer;              
+    this.source.connect(audioContext.destination);      
+    this.source.noteOn(0);      
   };
   
-  return AudioFile;
+  Sample.prototype.getInstrument = function() {
+    return this.name.match(/^(.+)(?:-chunk).+$/)[0];
+  };
+  
+  return Sample;
 })();
 
+stopAll = function() {
+  _.each(samples, function(sample) {
+    if (sample.source) sample.source.noteOff(0);
+  });
+}
 
+var currentSliceTicker = -1;
+var playInterval;
+var view;
+var samples;
 setupAPIs(function() {
   $(document).ready(function() {
     // initCamera(function() {
       
-      var tracks = [];
-      tracks.push(new Track('Beat'));
-      tracks.push(new Track('Bass'));
-      tracks.push(new Track('Rythm Guitar'));
-      tracks.push(new Track('Other Guitar'));
-      tracks.push(new Track('Siren'));
-      tracks.push(new Track('Voice'));
-      tracks.push(new Track('Synth'));
-      tracks.push(new Track('Synth 2'));
-      tracks.push(new Track('Keys'));
+      samples = _.map(sampleURLs, function(sampleURL) {
+        return new Sample(sampleURL);
+      });
       
+      timeSlices = [];
+      var numOfTimeslices = 8;
+      for (var i=0; i < numOfTimeslices; i++) {
+        timeSlices.push(new TimeSlice(samples));
+      };
       
-      var i = -1;
-      setInterval(function() {
-        var trackNo = ++i % tracks[0].audioFiles.length;
-        _.each(tracks, function(track) {
-          track.playBar(trackNo);
-        });
-      }, 6650);
-
-
+      view = new View(timeSlices);
+      
+      playInterval = setInterval(playCurrentSlice, 6650);
+      playCurrentSlice();
   });
 });
 
+
+function playCurrentSlice() {        
+  var currentPos = ++currentSliceTicker % timeSlices.length;
+  timeSlices[currentPos].playActiveSamples();
+  view.highlightSlice(currentPos);
+}
+
+
+
+var sampleURLs = ['Bass-chunk-12.mp3',
+'Bass-chunk-13.mp3',
+'Bass-chunk-15.mp3',
+'Bass-chunk-21.mp3',
+'Bass-chunk-22.mp3',
+'Beat-chunk-10.mp3',
+'Beat-chunk-6.mp3',
+'Beat-chunk-7.mp3',
+'Beat-chunk-9.mp3',
+'Keys-chunk-10.mp3',
+'Keys-chunk-14.mp3',
+'Keys-chunk-4.mp3',
+'Keys-chunk-9.mp3',
+'Other Guitar-chunk-20.mp3',
+'Other Guitar-chunk-21.mp3',
+'Other Guitar-chunk-22.mp3',
+'Other Guitar-chunk-23.mp3',
+'Other Guitar-chunk-24.mp3',
+'Other Guitar-chunk-3.mp3',
+'Other Guitar-chunk-4.mp3',
+'Rythm Guitar-chunk-12.mp3',
+'Rythm Guitar-chunk-4.mp3',
+'Rythm Guitar-chunk-5.mp3',
+'Synth 2-chunk-10.mp3',
+'Synth 2-chunk-11.mp3',
+'Synth 2-chunk-12.mp3',
+'Synth 2-chunk-13.mp3',
+'Synth 2-chunk-14.mp3',
+'Synth-chunk-20.mp3',
+'Synth-chunk-21.mp3',
+'Synth-chunk-23.mp3',
+'Synth-chunk-24.mp3',
+'Voice-chunk-14.mp3',
+'Voice-chunk-15.mp3',
+'Voice-chunk-16.mp3',
+'Voice-chunk-17.mp3',
+'Voice-chunk-18.mp3',
+'Voice-chunk-19.mp3',
+'Voice-chunk-20.mp3',
+'Voice-chunk-21.mp3',
+'Voice-chunk-22.mp3',
+'Voice-chunk-23.mp3',
+'Voice-chunk-24.mp3',
+'Voice-chunk-5.mp3',
+'Voice-chunk-6.mp3',
+'Voice-chunk-7.mp3',
+'Voice-chunk-8.mp3'];
